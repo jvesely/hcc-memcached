974fe36174f1fee7c082246bac2aca355003a11a
Jan Vesely <jan.vesely@rutgers.edu>
2017-01-02 10:24:08 -0500

    process,gpu: Use bucket-size as work-group size
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- process.cpp
+++ process.cpp
@@ -55,11 +55,13 @@ int async_process_gpu(const params *p)
 	uint64_t addr = (uint64_t)&address;
 	uint64_t socket = p->gpu_socket;
 	socklen_t address_len = sizeof(address);
+	unsigned groups = (20480 / p->bucket_size);
 
-	auto textent = hc::extent<1>::extent(1);
-	parallel_for_each(textent, [&](hc::index<1> idx) [[hc]] {
+	auto textent = hc::extent<1>::extent(groups * p->bucket_size).tile(p->bucket_size);
+	parallel_for_each(textent, [&](hc::tiled_index<1> idx) [[hc]] {
 		while (p->on_switch) {
 			address_len = sizeof(address);
+			if (idx.local[0] == 0) {
 			ssize_t data_len = sc.send(SYS_recvfrom,
 			                           {(uint64_t)socket,
 			                            (uint64_t)buffer.data(),
@@ -72,6 +74,8 @@ int async_process_gpu(const params *p)
 				                     ::std::min<size_t>(data_len,
 				                               buffer.size()),
 						     0, addr, address_len});
+			}
+			idx.barrier.wait();
 		}
 	}).wait();
 	return 0;
