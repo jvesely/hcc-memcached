d27e0d1d97aea7faf60f507ffce29af60182802d
Jan Vesely <jan.vesely@rutgers.edu>
2017-01-04 14:34:01 -0500

    process-gpu: Use per-tile buffer
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- process-gpu.cpp
+++ process-gpu.cpp
@@ -13,32 +13,40 @@ int async_process_gpu(const params *p)
 		return -1;
 	// HCC is bad with global variables
 	auto &sc = syscalls::get();
-	::std::vector<char> buffer(p->buffer_size);
+
 	struct sockaddr_in address = {0};
 	uint64_t addr = (uint64_t)&address;
 	uint64_t socket = p->gpu_socket;
 	socklen_t address_len = sizeof(address);
-	unsigned groups = (20480 / p->bucket_size);
 
-	auto textent = hc::extent<1>::extent(groups * p->bucket_size).tile(p->bucket_size);
+	unsigned groups = (20480 / p->bucket_size);
+	using buffer_t = ::std::vector<char>;
+	::std::vector<buffer_t> buffers(groups, buffer_t(p->buffer_size));
+
+	auto textent = hc::extent<1>::extent(p->bucket_size).tile(p->bucket_size);
 	parallel_for_each(textent, [&](hc::tiled_index<1> idx) [[hc]] {
+		buffer_t &buffer = buffers[idx.tile[0]];
 		while (p->on_switch) {
 			address_len = sizeof(address);
+			volatile tile_static ssize_t data_len;
 			if (idx.local[0] == 0) {
-			ssize_t data_len = sc.send(SYS_recvfrom,
-			                           {(uint64_t)socket,
-			                            (uint64_t)buffer.data(),
-			                            buffer.size(), MSG_TRUNC,
-			                            addr,
+				data_len = sc.send(SYS_recvfrom,
+				                   {(uint64_t)socket,
+				                    (uint64_t)buffer.data(),
+				                     buffer.size(), MSG_TRUNC,
+			                             addr,
 			                            (uint64_t)&address_len});
-			if (data_len > 0)
-				sc.send(SYS_sendto, {(uint64_t)socket,
-				                     (uint64_t)buffer.data(),
-				                     ::std::min<size_t>(data_len,
-				                               buffer.size()),
-						     0, addr, address_len});
 			}
-			idx.barrier.wait();
+			idx.barrier.wait_with_tile_static_memory_fence();
+
+			size_t response_size =
+				::std::min<size_t>(data_len, buffer.size());
+			if (idx.local[0] == 0) {
+				sc.send(SYS_sendto, {(uint64_t)socket,
+			                             (uint64_t)buffer.data(),
+			                             response_size,
+					             0, addr, address_len});
+			}
 		}
 	}).wait();
 	return 0;
